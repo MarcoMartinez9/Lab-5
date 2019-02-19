0a30165c57fa79aa43ed83281fd500f194651ff6
MarcoMartinez9 <47228260+MarcoMartinez9@users.noreply.github.com>
2019-02-19 17:44:22 -0600

    Update Todo

--- Todo.java
+++ Todo.java
@@ -1,18 +1,18 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 11
+Last Modified_______: 2019-19-02
+OU ID_______________: 113424558
+OU EMAIL____________: marco.e.martinez-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Complete the implementation of the Calculator class.
+2. Complete the implementation of the CalculatorTest class.
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 60
+2. 60
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 60
+2. 50
 */}
